f9c1ad8d28b7979f75f7bdac4d9480ac3464da7d
nathanlin-a11y <191123524+nathanlin-a11y@users.noreply.github.com>
2024-07-25 15:16:22 -0300

    Frontend: updated the register flow, some ui improvements. Still need to update the EnhancedSelect's design. Backend: Minor updates to tasks about typing the api engine instead of having different classes of tasks for different apis. Workflow: Updated how apis are consumed for apitasks, removing the need for multiple classes and instead created an 'apiengine' class to provide the 'generate_response' for the task. Updated the initialization logic to enable multiple modules with dependencies to destructure the initializated elements to allow for user customization and additions

--- workflow_logic/core/__init__.py
+++ workflow_logic/core/__init__.py
@@ -4,13 +4,12 @@ from .chat_functionality import ChatExecutionFunctionality
 from .model import PIKAModel
 from .parameters import ParameterDefinition, FunctionParameters
 from .prompt import Prompt
-from .tasks import PIKATask, Workflow, BasicAgentTask, PromptAgentTask, APITask, Workflow, RedditSearchTask, ExaSearchTask, WikipediaSearchTask, GoogleSearchTask, ArxivSearchTask, CodeGenerationLLMTask, CodeExecutionLLMTask, CheckTask
+from .tasks import PIKATask, Workflow, BasicAgentTask, PromptAgentTask, APITask, Workflow, CodeGenerationLLMTask, CodeExecutionLLMTask, CheckTask
 from .communication import TaskResponse, DatabaseTaskResponse, OutputInterface, SearchResult, StringOutput, LLMChatOutput, SearchOutput, WorkflowOutput, MessageDict
 from .tasks.templated_task import TemplatedTask
 from .api import APIManager, API
 
 __all__ = ['PIKATask', 'Workflow', 'PIKAAgent', 'PIKAModel', 'BasicAgentTask', 'PromptAgentTask', 'APITask', 
-           'TemplatedTask', 'Workflow', 'CVGenerationTask', 'RedditSearchTask', 'ExaSearchTask', 'WikipediaSearchTask', 'GoogleSearchTask',
-           'ArxivSearchTask', 'CodeGenerationLLMTask', 'CodeExecutionLLMTask', 'CheckTask', 'Prompt', 'PIKAChat', 
+           'TemplatedTask', 'Workflow', 'CVGenerationTask', 'CodeGenerationLLMTask', 'CodeExecutionLLMTask', 'CheckTask', 'Prompt', 'PIKAChat', 
             'ParameterDefinition', 'FunctionParameters', 'TaskResponse', 'DatabaseTaskResponse', 'MessageDict', 'ChatExecutionFunctionality',
            'OutputInterface', 'SearchResult', 'StringOutput', 'LLMChatOutput', 'SearchOutput', 'WorkflowOutput', 'APIManager', 'API']
--- workflow_logic/core/tasks/agent_tasks/__init__.py
+++ workflow_logic/core/tasks/agent_tasks/__init__.py
@@ -1,6 +1,5 @@
 from .agent_task import BasicAgentTask
 from .prompt_agent_task import  PromptAgentTask, CheckTask, CodeExecutionLLMTask, CodeGenerationLLMTask, TemplatedTask
 
-__all__ = ['PIKATask', 'Workflow', 'PIKAAgent','BasicAgentTask', 'PromptAgentTask', 'APITask', 'TemplatedTask', 'RedditSearchTask', 
-           'GoogleSearchTask', 'WikipediaSearchTask', 'ExaSearchTask', 'APISearchTask', 'SearchOutput', 'SearchResult', 'ArxivSearchTask', 
-           'CheckTask', 'CodeExecutionLLMTask', 'CodeGenerationLLMTask', 'CVGenerationTask']
+__all__ = ['PIKATask', 'Workflow', 'PIKAAgent','BasicAgentTask', 'PromptAgentTask', 'APITask', 'TemplatedTask', 
+           'SearchOutput', 'SearchResult', 'CheckTask', 'CodeExecutionLLMTask', 'CodeGenerationLLMTask', 'CVGenerationTask']
