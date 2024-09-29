50a5d96ed990192c034d9612b676fa6e39f5716a
nathanlin-a11y <191123524+nathanlin-a11y@users.noreply.github.com>
2024-09-29 18:52:04 -0300

    A lot of work in Workflow, creating a single References class to store all types of data, like msg, files, task results, etc. Refactored everything to now work with References instead of directly with messages so much, and now msg and task responses have a reference object where they can store any kind of data. The Reference class offers a clear way to represent everything as a string, since that's the idea. Updated backend and a bit of frontend, but a lot of adustments missing

--- workflow_logic/__init__.py
+++ workflow_logic/__init__.py
@@ -1,6 +1,6 @@
-from .core import PIKATask, Workflow, PIKAAgent, PIKAModel, Prompt, PIKAChat, MessageDict, DatabaseTaskResponse
+from .core import PIKATask, Workflow, PIKAAgent, PIKAModel, Prompt, PIKAChat, MessageDict, TaskResponse
 from .api_app import WORKFLOW_APP
 from .db_app import BackendAPI, ContainerAPI, DB_STRUCTURE, DBInitManager, DBStructure
 
 __all__ = ['PIKATask', 'Workflow', 'PIKAAgent', 'PIKAModel', 'TaskResponse', 'DB_STRUCTURE', 'DBInitManager', 'DBStructure',
-           'FunctionConfig', 'MessageDict', 'DatabaseTaskResponse','PIKAChat', 'WORKFLOW_APP', 'BackendAPI', 'ContainerAPI', 'Prompt']
+           'FunctionConfig', 'MessageDict', 'TaskResponse','PIKAChat', 'WORKFLOW_APP', 'BackendAPI', 'ContainerAPI', 'Prompt']
--- workflow_logic/core/tasks/agent_tasks/__init__.py
+++ workflow_logic/core/tasks/agent_tasks/__init__.py
@@ -1,5 +1,5 @@
-from .agent_task import BasicAgentTask, FileTask
+from .agent_task import BasicAgentTask
 from .prompt_agent_task import  PromptAgentTask, CheckTask, CodeGenerationLLMTask, CodeExecutionLLMTask
 
-__all__ = ['PIKATask', 'Workflow', 'PIKAAgent','BasicAgentTask', 'PromptAgentTask', 'APITask', 'FileTask',
+__all__ = ['PIKATask', 'Workflow', 'PIKAAgent','BasicAgentTask', 'PromptAgentTask', 'APITask', 
             'CheckTask', 'CodeExecutionLLMTask', 'CodeGenerationLLMTask', 'CVGenerationTask']
