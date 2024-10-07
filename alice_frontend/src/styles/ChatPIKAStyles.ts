0d1d32a9ddbcc9a49003a34a4c861bd8c402270e
nathanlin-a11y <191123524+nathanlin-a11y@users.noreply.github.com>
2024-10-06 21:34:37 -0300

    Workflow: Added 2 workflows, implemented the webscraping logic. Frontend: Slight fixes to chat and messages

--- alice_frontend/src/styles/ChatPIKAStyles.ts
+++ alice_frontend/src/styles/ChatPIKAStyles.ts
@@ -5,19 +5,22 @@ const useStyles = makeStyles((theme: Theme) => ({
   chatPIKAContainer: {
     display: 'flex',
     height: '100%',
+    maxWidth: '100%',
+    // overflow: 'hidden',
   },
   chatPIKAMain: {
     flexGrow: 1,
     display: 'flex',
     flexDirection: 'column',
-    // maxWidth: '90%',
+    minWidth: 0,
+    overflow: 'auto',
   },
   chatPIKAMessages: {
     flexGrow: 1,
-    overflowY: 'auto',
-    paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1),
-    paddingTop: theme.spacing(1),
+    minWidth: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '0 10px'
   },
   chatPIKAInput: {
     borderTop: `1px solid ${theme.palette.divider}`,
