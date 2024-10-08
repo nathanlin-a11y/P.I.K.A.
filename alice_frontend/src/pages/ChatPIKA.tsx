b21de0abcc68c1b485db38c919e291642cf59ff7
nathanlin-a11y <191123524+nathanlin-a11y@users.noreply.github.com>
2024-10-08 16:13:33 -0300

    Fixes for merge

--- alice_frontend/src/pages/ChatPIKA.tsx
+++ alice_frontend/src/pages/ChatPIKA.tsx
@@ -189,7 +189,7 @@ const ChatPIKA: React.FC = () => {
         )()}
       </Box>
     )
-  }, [selectChatId, currentChat, checkAndAddTask, addTaskResponse, addFileReference, addURLReference, addMessageReference, selectCardItem, pastChats]);
+  }, [selectChatId, currentChat, checkAndAddTask, addTaskResponse, addFileReference, addURLReference, addMessageReference, selectCardItem, pastChats, classes.activeListContainer]);
 
   return (
     <Box className={classes.chatPIKAContainer}>
